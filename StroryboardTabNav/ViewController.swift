c293f6269fa431d89164bf74699c5328be6366d4
杜博 <905517804@qq.com>
2017-02-09 17:38:17 +0800

    Initial Commit

---
+++ StroryboardTabNav/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StroryboardTabNav
+//
+//  Created by 杜博 on 2017/2/9.
+//  Copyright © 2017年 杜博. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
